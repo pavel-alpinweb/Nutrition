998ad2c38a333b8e3bfafcd8338983444d6c840c
pavel-alpinweb <sioda@xfamily.io>
2022-05-12 14:54:26 +0300

    create dish select component

--- src/components/DishSelect.vue
+++ src/components/DishSelect.vue
@@ -3,8 +3,33 @@
     <Card class="p-mb-2">
       <template #content>
         <div class="p-grid">
-          <div class="p-col-6">Блюдо</div>
-          <div class="p-col-6">Количество</div>
+          <div class="p-col-6">
+            <div class="p-grid p-fluid">
+              <div class="p-col-12">
+                <Dropdown
+                  :filter="true"
+                  optionLabel="name"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="p-col-6">
+            <div class="p-grid p-fluid">
+              <div class="p-col-11">
+                <InputText
+                  class="ingredient__text"
+                  type="number"
+                  min="1"
+                />
+              </div>
+              <div class="p-col-1">
+                <Button
+                  icon="fas fa-trash"
+                  class="p-button-rounded p-button-danger"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </template>
     </Card>
@@ -13,11 +38,17 @@
 
 <script>
 import Card from 'primevue/card';
+import Dropdown from 'primevue/dropdown';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 
 export default {
   name: 'DishSelect',
   components: {
     Card,
+    Dropdown,
+    InputText,
+    Button,
   },
 };
 </script>

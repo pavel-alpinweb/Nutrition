1d4a13fca693a79e643ea40b5e8ad121026dd906
pavel-alpinweb <sioda@xfamily.io>
2022-05-13 14:17:48 +0300

    create base layout for page

--- src/pages/menus/menusPickProductList.vue
+++ src/pages/menus/menusPickProductList.vue
@@ -1,13 +1,44 @@
 <template>
-  <h1>Menus Pick Product List</h1>
+  <defaultPageLayout>
+    <template v-slot:page-content>
+      <div class="menus-pick-product-list">
+        <div class="menus-pick-product-list__header">
+          <div class="menus-pick-product-list__wrapper-form p-formgrid">
+            <h1>menus-pick-product-list</h1>
+          </div>
+        </div>
+        <div class="menus-pick-product-list__content">
+          <h2>menus-pick-product-list</h2>
+        </div>
+      </div>
+    </template>
+  </defaultPageLayout>
 </template>
 
 <script>
+import defaultPageLayout from '@/layouts/DefaultPageLayout.vue';
+
 export default {
   name: 'menusPickProductList',
+  components: {
+    defaultPageLayout,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.menus-pick-product-list {
+  padding: 10px;
+  &__header {
+    display: flex;
+    justify-content: flex-end;
+  }
+  &__wrapper-form {
+    flex-basis: 50%;
+  }
+  &__btn-container {
+    display: flex;
+    align-items: flex-end;
+  }
+}
 </style>

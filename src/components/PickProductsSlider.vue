cd591a88cd10c842f4c14eef15f82453ca98522a
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-05-25 11:50:06 +0600

    redesign PickProductSlider

--- src/components/PickProductsSlider.vue
+++ src/components/PickProductsSlider.vue
@@ -1,37 +1,57 @@
 <template>
-  <Carousel
-    class="pick-product-slider"
-    :value="category.products"
-    :circular="true"
-    :numVisible="3"
-    :numScroll="3"
-  >
-    <template #header>
-      <h2 class="pick-product-slider__title">{{ category.productCategory }}</h2>
-    </template>
-    <template #item="slotProps">
-      <div class="pick-product-slider__item">
-        <NutritionCard
-          :item="slotProps.data"
-          :is-pick-product="true"
-          @checkProductEvent="checkProductHandler"
-        />
-      </div>
-    </template>
-  </Carousel>
+<!--  <Carousel-->
+<!--    class="pick-product-slider"-->
+<!--    :value="category.products"-->
+<!--    :circular="true"-->
+<!--    :numVisible="3"-->
+<!--    :numScroll="3"-->
+<!--  >-->
+<!--    <template #header>-->
+<!--      <h2 class="pick-product-slider__title">{{ category.productCategory }}</h2>-->
+<!--    </template>-->
+<!--    <template #item="slotProps">-->
+<!--      <div class="pick-product-slider__item">-->
+<!--        <NutritionCard-->
+<!--          :item="slotProps.data"-->
+<!--          :is-pick-product="true"-->
+<!--          @checkProductEvent="checkProductHandler"-->
+<!--        />-->
+<!--      </div>-->
+<!--    </template>-->
+<!--  </Carousel>-->
+  <div class="pick-product-slider">
+    <h2 class="pick-product-slider__title">{{ category.productCategory }}</h2>
+    <div class="pick-product-slider__content">
+      <ScrollPanel style="width: 100%; height: 700px" class="custombar1">
+        <div class="pick-product-slider__content">
+          <div
+            v-for="item in category.products"
+            :key="item.id"
+            class="pick-product-slider__item"
+          >
+            <NutritionCard
+              :item="item"
+              :is-pick-product="true"
+              @checkProductEvent="checkProductHandler"
+            />
+          </div>
+        </div>
+      </ScrollPanel>
+    </div>
+  </div>
 </template>
 
 <script>
-import Carousel from 'primevue/carousel';
 import NutritionCard from '@/components/NutritionCard.vue';
+import ScrollPanel from 'primevue/scrollpanel';
 import { useStore } from 'vuex';
 import { eventBus } from '@/modules/utils';
 
 export default {
   name: 'PickProductsSlider',
   components: {
-    Carousel,
     NutritionCard,
+    ScrollPanel,
   },
   props: {
     category: {
@@ -67,12 +87,30 @@ export default {
     &__title {
       text-align: center;
     }
-    &__item {
-      width: 100%;
-      max-width: 300px;
-      margin: 0 auto;
+    &__content {
       display: flex;
       justify-content: center;
+      flex-wrap: nowrap;
+    }
+    &__item {
+      min-width: 300px;
+      flex-basis: 300px;
+      margin: 0 15px;
+      display: flex;
+      justify-content: center;
+    }
+  }
+  ::v-deep(.p-scrollpanel) {
+    &.custombar1 {
+      .p-scrollpanel-bar {
+        background-color: #FFBA01;
+        opacity: 1;
+        transition: background-color .2s;
+
+        &:hover {
+          background-color: #FFBA01;
+        }
+      }
     }
   }
 </style>

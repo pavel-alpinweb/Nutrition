bcf92284a6ce471229a550b9d393c5da9d1588e6
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-05-26 12:34:43 +0600

    create menu cost input

--- src/components/MenuGenerator.vue
+++ src/components/MenuGenerator.vue
@@ -8,7 +8,20 @@
     :dismissableMask="true"
   >
     <div class="menu-generator">
-      <div class="menu-generator__content"></div>
+      <div class="menu-generator__content">
+        <div class="menu-generator__section">
+          <div class="p-fluid p-field">
+            <label for="cost">Максимальное допустимая стоимость меню</label>
+            <InputText
+              v-model="menuCost"
+              id="cost"
+              type="number"
+              placeholder="Стоимость"
+              min="1"
+            />
+          </div>
+        </div>
+      </div>
       <div class="menu-generator__footer">
         <Button label="Отмена" />
         <Button label="Подобрать" class="p-button-success"/>
@@ -20,6 +33,7 @@
 <script>
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 import { ref, onMounted } from 'vue';
 import { eventBus } from '@/modules/utils';
 
@@ -28,9 +42,11 @@ export default {
   components: {
     Dialog,
     Button,
+    InputText,
   },
   setup() {
     const showMenuGenerator = ref(false);
+    const menuCost = ref(1);
 
     onMounted(() => {
       eventBus.on('showMenuGenerator', () => {
@@ -40,6 +56,7 @@ export default {
 
     return {
       showMenuGenerator,
+      menuCost,
     };
   },
 };

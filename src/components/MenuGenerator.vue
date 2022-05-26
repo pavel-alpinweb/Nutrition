715c3de1a758c8c89b5c489007a9e984740a274b
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-05-26 12:25:21 +0600

    create menu-generator__footer

--- src/components/MenuGenerator.vue
+++ src/components/MenuGenerator.vue
@@ -7,12 +7,19 @@
     :draggable="false"
     :dismissableMask="true"
   >
-    content
+    <div class="menu-generator">
+      <div class="menu-generator__content"></div>
+      <div class="menu-generator__footer">
+        <Button label="Отмена" />
+        <Button label="Подобрать" class="p-button-success"/>
+      </div>
+    </div>
   </Dialog>
 </template>
 
 <script>
 import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 import { ref, onMounted } from 'vue';
 import { eventBus } from '@/modules/utils';
 
@@ -20,6 +27,7 @@ export default {
   name: 'MenuGenerator',
   components: {
     Dialog,
+    Button,
   },
   setup() {
     const showMenuGenerator = ref(false);
@@ -37,6 +45,11 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .menu-generator {
+    &__footer {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 </style>

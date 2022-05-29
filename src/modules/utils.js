438c858af91de8ea7be7314677fd5a3bae8df268
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-05-29 17:55:53 +0600

    create downloadFile function

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,4 +1,29 @@
 import mitt from 'mitt';
 
-// eslint-disable-next-line import/prefer-default-export
 export const eventBus = mitt();
+
+export function downloadFile(data, filename, mime) {
+  // It is necessary to create a new blob object with mime-type explicitly set
+  // otherwise only Chrome works like it should
+  const blob = new Blob([data], { type: mime || 'application/octet-stream' });
+  // Create a link pointing to the ObjectURL containing the blob
+  const blobURL = window.URL.createObjectURL(blob);
+  const tempLink = document.createElement('a');
+  tempLink.style.display = 'none';
+  tempLink.href = blobURL;
+  tempLink.setAttribute('download', filename);
+  // Safari thinks _blank anchor are pop ups. We only want to set _blank
+  // target if the browser does not support the HTML5 download attribute.
+  // This allows you to download files in desktop safari if pop up blocking
+  // is enabled.
+  if (typeof tempLink.download === 'undefined') {
+    tempLink.setAttribute('target', '_blank');
+  }
+  document.body.appendChild(tempLink);
+  tempLink.click();
+  document.body.removeChild(tempLink);
+  setTimeout(() => {
+    // For Firefox it is necessary to delay revoking the ObjectURL
+    window.URL.revokeObjectURL(blobURL);
+  }, 100);
+}

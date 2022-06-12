0484952e0fdda32317226a9524b2b9a6bb07f023
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-06-12 18:49:40 +0600

    create default generatorParams & bindings data

--- src/components/MenuGenerator.vue
+++ src/components/MenuGenerator.vue
@@ -13,7 +13,7 @@
           <div class="p-fluid p-field">
             <label for="name">Название меню</label>
             <InputText
-              v-model="menuName"
+              v-model="generatorParams.menuName"
               id="name"
               type="text"
               placeholder="Название"
@@ -22,7 +22,7 @@
           <div class="p-fluid p-field">
             <label for="cost">Максимальное допустимая стоимость меню</label>
             <InputText
-              v-model="menuCost"
+              v-model="generatorParams.maxPrice"
               id="cost"
               type="number"
               placeholder="Стоимость"
@@ -34,7 +34,7 @@
               <div class="p-fluid p-field">
                 <label for="minMealsNumber">Минимальное кол-во приемов пищи на человека</label>
                 <InputText
-                  v-model="minMealsNumber"
+                  v-model="generatorParams.minMealsNumber"
                   id="minMealsNumber"
                   type="number"
                   placeholder="Стоимость"
@@ -46,7 +46,7 @@
               <div class="p-fluid p-field">
                 <label for="minMealsNumber">Кол-во порций блюд в одном приеме пищи</label>
                 <InputText
-                  v-model="servingNumberPerMeal"
+                  v-model="generatorParams.servingNumberPerMeal"
                   id="servingNumberPerMeal"
                   type="number"
                   placeholder="Стоимость"
@@ -65,7 +65,11 @@
             />
           </label>
           <div class="p-formgrid">
-            <div class="p-fluid p-grid">
+            <div
+              v-for="(product, key) in generatorParams.productConstraints"
+              :key="key"
+              class="p-fluid p-grid"
+            >
               <div class="p-field p-col-6">
                 <Dropdown
                   optionLabel="name"
@@ -80,6 +84,7 @@
               </div>
               <div class="p-field p-col-2">
                 <InputText
+                  v-model="product.quantity"
                   type="number"
                   placeholder="Кол-во"
                   min="1"
@@ -103,7 +108,11 @@
             />
           </label>
           <div class="p-formgrid">
-            <div class="p-fluid p-grid">
+            <div
+              v-for="(dish, key) in generatorParams.dishTagConstraints"
+              :key="key"
+              class="p-fluid p-grid"
+            >
               <div class="p-field p-col-6">
                 <Dropdown
                   optionLabel="name"
@@ -118,6 +127,7 @@
               </div>
               <div class="p-field p-col-2">
                 <InputText
+                  v-model="dish.quantity"
                   type="number"
                   placeholder="Кол-во"
                   min="1"
@@ -145,7 +155,7 @@ import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Dropdown from 'primevue/dropdown';
-import { ref, onMounted } from 'vue';
+import { reactive, ref, onMounted } from 'vue';
 import { eventBus } from '@/modules/utils';
 
 export default {
@@ -158,10 +168,46 @@ export default {
   },
   setup() {
     const showMenuGenerator = ref(false);
-    const menuCost = ref(1);
-    const menuName = ref('');
-    const minMealsNumber = ref(1);
-    const servingNumberPerMeal = ref(1);
+    const generatorParams = reactive({
+      menuName: 'string',
+      maxPrice: 1,
+      minMealsNumber: 1,
+      servingNumberPerMeal: 1,
+      productConstraints: [
+        {
+          productCategory: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+        {
+          productCategory: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+        {
+          productCategory: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+        {
+          productCategory: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+      ],
+      dishTagConstraints: [
+        {
+          dishTag: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+        {
+          dishTag: 'string',
+          condition: 'lessOrEqual',
+          quantity: 0,
+        },
+      ],
+    });
 
     onMounted(() => {
       eventBus.on('showMenuGenerator', () => {
@@ -171,10 +217,7 @@ export default {
 
     return {
       showMenuGenerator,
-      menuCost,
-      menuName,
-      minMealsNumber,
-      servingNumberPerMeal,
+      generatorParams,
     };
   },
 };

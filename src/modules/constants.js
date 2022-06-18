1ad1707da02404e52f13dfe924d2904a0c395ac1
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2022-06-18 15:47:07 +0600

    change server's protocol

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
-export const BASE_API_URL = 'http://nutritionmanager.xyz';
+export const BASE_API_URL = 'https://nutritionmanager.xyz';
 export const GLOBAL_UNITS = [
   { name: 'кг', code: 'kg' },
   { name: 'литр', code: 'kg2' },
